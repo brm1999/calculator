f3bcef1346160d3cb9d43acb2ef93875d10da3f8
PratyushGolait2611 <69040672+PratyushGolait2611@users.noreply.github.com>
2020-10-01 14:16:14 +0530

    Update calculator.java

--- src/calculator.java
+++ src/calculator.java
@@ -1,19 +1,31 @@
 //author : Bishal Raj Majumder
 //Java code for a calculator
 
+// Importing Java math libraries
 import java.util.*;
+
+// Created a class named Calculator
 public class calculator {
 
-public static void main(String[] args) {
+    
+ public static void main(String[] args) {
 
+     // Used scanner to get input
     Scanner sc = new Scanner(System.in);
+     
+    // Instructions
     System.out.print("\nEnter two numbers: ");
+     
     // nextDouble() reads the next double from the keyboard
     double first = sc.nextDouble();
     double second = sc.nextDouble();
+     
+     // Instructions
     System.out.print("\nEnter an operator (+, -, *, /): ");
     char op = sc.next().charAt(0);
     double result;
+     
+     // Used a switch case
     switch(op)
     {
         case '+':
@@ -38,6 +50,7 @@ public static void main(String[] args) {
                 return;
         }
 
+        // To display results
         System.out.printf("\n%.1f %c %.1f = %.1f", first, op, second, result);
     }
 }
